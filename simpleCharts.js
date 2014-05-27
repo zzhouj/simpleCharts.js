77885832e42cdb1e7c10b84b9b4c07c5be7956e4
justin <zzhouj@yahoo.com>
2014-05-27 09:34:02 +0800

    calcYAxisRange

--- simpleCharts.js
+++ simpleCharts.js
@@ -18,7 +18,10 @@
         options.xAxis.color = options.xAxis.color || '#808080';
         options.yAxis = options.yAxis || {};
         options.yAxis.color = options.yAxis.color || '#808080';
+        options.yAxis.lineColor = options.yAxis.lineColor || '#808080';
+        options.yAxis.step = options.yAxis.step || 5;
 
+        // canvas
         options.width = canvas.width;
         options.height = canvas.height;
         options.ctx = canvas.getContext('2d');
@@ -29,7 +32,30 @@
     };
 
     calcYAxisRange = function (options) {
-        // TODO
+        var data = options.series.data;
+        if (data.length <= 0) {
+            options.yAxis.range = [0, 100];
+        } else {
+            var min = data[0];
+            var max = data[0];
+            for (var i = 1; i < data.length; i++) {
+                if (min > data[i]) {
+                    min = data[i];
+                }
+                if (max < data[i]) {
+                    max = data[i];
+                }
+            }
+            var step = options.yAxis.step;
+            while ((options.height / ((max - min) / step + 3)) < options.fontSize) {
+                step *= 2;
+            }
+            min = Math.floor(min / step) * step;
+            options.yAxis.range = [];
+            for (; min <= max; min += step) {
+                options.yAxis.range.push(min);
+            }
+        }
     };
 
     drawSimpleCharts = function (options) {
